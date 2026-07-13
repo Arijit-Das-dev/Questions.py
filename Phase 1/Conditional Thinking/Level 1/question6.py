f2fbd196998e34a758559b5b9310ba32b8b0b950
Arijit Das <arijitdas.das99@gmail.com>
2026-07-13 20:55:43 +0530

    6. Take two numbers and print the larger one.

---
+++ Phase 1/Conditional Thinking/Level 1/question6.py
@@ -0,0 +1,9 @@
+# 6. Take two numbers and print the larger one. 
+
+num1 = int(input("Enter first number : "))
+num2= int(input("Enter second number : "))
+
+if num1 > num2:
+    print(f"{num1} is greater")
+else:
+    print(f"{num2} is greater")
