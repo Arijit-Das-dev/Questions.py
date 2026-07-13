8644e55204db0f16ff83e21e486806e1285b83b7
Arijit Das <arijitdas.das99@gmail.com>
2026-07-13 20:36:45 +0530

    print whether it’s positive, negative, or zero.

---
+++ Phase 1/Conditional Thinking/Level 1/questions1.py
@@ -0,0 +1,10 @@
+# 1. Take a number and print whether it’s positive, negative, or zero. 
+
+num = int(input("Enter a number : "))
+
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
+else:
+    print("Zero")
