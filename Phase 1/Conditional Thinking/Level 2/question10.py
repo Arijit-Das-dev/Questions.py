e4b8017100c8c66cf4292df75e3ced44b07bd194
Arijit Das <arijitdas.das99@gmail.com>
2026-07-14 12:14:26 +0530

    10. Take a month number (1–12) and print the number of days in that month (ignore leap years).

--- Phase 1/Conditional Thinking/Level 2/question10.py
+++ Phase 1/Conditional Thinking/Level 2/question10.py
@@ -1,2 +1,15 @@
 # 10. Take a month number (1–12) and print the number of days in that month (ignore leap years).
 
+month = int(input("Enter month number (1-12): "))
+
+if month < 1 or month > 12:
+    print("Invalid month")
+
+elif month == 2:
+    print("28 days")
+
+elif month == 4 or month == 6 or month == 9 or month == 11:
+    print("30 days")
+
+else:
+    print("31 days")
