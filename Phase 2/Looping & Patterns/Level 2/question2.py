f071b67b15f5429a9894f3fd8767533ecb623450
Arijit Das <arijitdas.das99@gmail.com>
2026-07-16 20:08:23 +0530

    2. Print the reverse of a given number.

---
+++ Phase 2/Looping & Patterns/Level 2/question2.py
@@ -0,0 +1,13 @@
+# 2. Print the reverse of a given number.
+
+num = int(input("Enter a number : "))
+
+rev = 0
+
+while num!= 0:
+
+    rem = num % 10
+    rev = rev*10 + rem
+    num //= 10
+
+print(rev)
