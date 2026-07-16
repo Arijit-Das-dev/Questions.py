1c1eba6201a6a55bd35dbcfb4713a50e2c9b08a1
Arijit Das <arijitdas.das99@gmail.com>
2026-07-16 14:29:24 +0530

    2. Print all even numbers between 1 and 100.

---
+++ Phase 2/Looping & Patterns/Level 1/question2.py
@@ -0,0 +1,15 @@
+# 2. Print all even numbers between 1 and 100. 
+
+# using for loop
+result = [num for num in range(1, 101) if num%2 ==0]
+print(result)
+
+# using while loop
+i = 1
+
+while i< 101:
+
+    if i%2 == 0:
+        print(i, end=" ")
+    
+    i += 1
