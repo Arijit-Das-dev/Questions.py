a2fdf00b8edc56d16e3e4baba9ce93c1be035467
Arijit Das <arijitdas.das99@gmail.com>
2026-07-16 13:32:31 +0530

    6. Take two numbers and check if both are positive and their sum is less than 100.

---
+++ Phase 1/Conditional Thinking/Level 4/question6.py
@@ -0,0 +1,10 @@
+# 6. Take two numbers and check if both are positive and their sum is less than 100.
+
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+
+if num1 > 0 and num2 > 0 and (num1 + num2) < 100:
+    print(True)
+
+else:
+    print(False)
