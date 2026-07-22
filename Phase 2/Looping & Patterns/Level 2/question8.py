7899e296f25fcaf67328e7b5b7e39d3a58a0b9d9
Arijit Das <arijitdas.das99@gmail.com>
2026-07-22 20:11:39 +0530

    7. Print all prime numbers between 1 and 100.

--- Phase 2/Looping & Patterns/Level 2/question8.py
+++ Phase 2/Looping & Patterns/Level 2/question8.py
@@ -1,1 +1,18 @@
 # 7. Print all prime numbers between 1 and 100. 
+
+for num in range(1, 101):
+
+    if num < 2:
+        continue
+
+    is_prime = True
+
+    for j in range(2, num):
+        if num%j == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print(num)
+    else:
+        continue
