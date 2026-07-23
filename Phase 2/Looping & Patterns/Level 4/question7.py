3109fb0a36f9582e085a73c8977a5be00432eec6
Arijit Das <arijitdas.das99@gmail.com>
2026-07-23 16:42:09 +0530

    7. Print a pattern where each row i prints i*i.

--- Phase 2/Looping & Patterns/Level 4/question7.py
+++ Phase 2/Looping & Patterns/Level 4/question7.py
@@ -5,3 +5,9 @@ for i in range(1, 6):
     for j in range(1, 6):
         print(i*i, end=" ")
     print()
+
+for i in range(1, 6):
+
+    for j in range(1, i+1):
+        print(i*i, end=" ")
+    print()
