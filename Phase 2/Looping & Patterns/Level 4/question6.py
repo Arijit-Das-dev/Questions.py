f1962e71350eb6cce5214f24e43313d8214e854b
Arijit Das <arijitdas.das99@gmail.com>
2026-07-24 22:19:27 +0530

    1. Print Square of Stars (n x n Stars)

---
+++ Phase 2/Looping & Patterns/Level 4/question6.py
@@ -0,0 +1,1 @@
+# 6. Print all numbers from 1–n whose binary representation has an even number of 1s. 
---
+++ Phase 2/Looping & Patterns/Level 6/question1.py
@@ -0,0 +1,7 @@
+# 1. Print Square of Stars (n x n Stars)
+# Code:
+# * * * *
+# * * * *
+# * * * *
+# * * * *
+
