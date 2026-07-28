22b28a32f6eb154316d0d049dbf6fbe5eeef789a
Arijit Das <arijitdas.das99@gmail.com>
2026-07-28 22:19:19 +0530

    Print a Right-Aligned Triangle of stars

--- Phase 2/Looping & Patterns/Level 6/question3.py
+++ Phase 2/Looping & Patterns/Level 6/question3.py
@@ -5,6 +5,8 @@
 #     * * * 
 #   * * * * 
 # * * * * *
+
+# method 1
 rows = 5
 
 for i in range(1, rows+1):
@@ -13,4 +15,17 @@ for i in range(1, rows+1):
         print(" ", end=" ")
     for k in range(1, i+1):
         print("*", end=" ")
+    print()
+
+# method 2
+n = 5
+
+for i in range(1, n+1):
+
+    for j in range(i, n):
+        print(" ", end=" ")
+
+    for k in range(i):
+        print("*", end=" ")
+
     print()
